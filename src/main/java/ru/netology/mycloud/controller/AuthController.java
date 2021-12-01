8d2aaae4e14758ba3c6f4327558b21353bcc0ad1
AChebykin <riddle1994@ya.ru>
2021-12-01 18:37:12 +0300

    added webConfig

--- src/main/java/ru/netology/mycloud/controller/AuthController.java
+++ src/main/java/ru/netology/mycloud/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +20,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
